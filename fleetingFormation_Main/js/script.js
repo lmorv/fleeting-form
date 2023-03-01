6a8895346c7d9755c1958a2397f2211c51fe4918
lmorv <kishongus@gmail.com>
2023-03-01 18:09:00 -0500

    FleetForm: Created main branch.

---
+++ fleetingFormation_Main/js/script.js
@@ -0,0 +1,32 @@
+var video;
+var vScale = 16;
+
+var particles = [];
+
+
+var timer = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(1);
+  video = createCapture(VIDEO);
+  video.size(width / vScale, height / vScale);
+  for (var i = 0; i < 200; i++) {
+    particles[i] = new Particle (random(width), random(height));
+  }
+  
+video.hide();
+  background(0);
+  
+//   background(51);
+}
+
+function draw() {
+  video.loadPixels(); 
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].show();
+
+    
+  }
+}
